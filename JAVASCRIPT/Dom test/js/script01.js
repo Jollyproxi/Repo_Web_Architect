91f6cd836f873083edf6ddb3c68287f83542d216
Jollyproxi <davide.giolitti@edu-its.it>
2026-04-07 12:25:12 +0200

    Add book list and querySelectorAll examples
    Add a "Lista Libri" <ul> with several <li> items (classes red/blue and ids l1..l6) to dom.html. Update script01.js to remove stray blank lines and add examples using querySelectorAll: select all <li>, .blue, #listaLibri li, and #listaLibri li.red; log types and iterate NodeLists with forEach to demonstrate the difference between HTMLCollection and NodeList for DOM selection/iteration (educational/demo purposes).

--- JAVASCRIPT/Dom test/js/script01.js
+++ JAVASCRIPT/Dom test/js/script01.js
@@ -3,8 +3,6 @@ console.log(listaSpesa);
 console.log(typeof listaSpesa);
 
 
-
-
 // HTML collection
 let listaBlue = document.getElementsByClassName("blue");
 console.log(listaBlue);
@@ -20,7 +18,38 @@ let listaCose = document.querySelector("#listaSpesa");
 console.log("Lista spesa", listaCose);
 
 
-
 let itemBlue = document.querySelector(".blue");
 console.log("Item blue", itemBlue);
 
+// .querySelectorAll raccoglie tutti gli elementi che corrispondono al selettore creando una Nodelist chè un
+// parete più stretto delle HTLM collection
+let lis = document.querySelectorAll("li");
+console.log(lis);
+console.log(typeof lis);
+lis.forEach(li => {
+    console.log(li);
+})
+
+
+let lisBlue = document.querySelectorAll(".blue");
+console.log(lisBlue);
+console.log(typeof lisBlue);
+lisBlue.forEach(li => {
+    console.log(li);
+})
+
+
+let lisLibri = document.querySelectorAll("#listaLibri li");
+console.log(lisLibri);
+console.log(typeof lisLibri);
+lisLibri.forEach(li => {
+    console.log(li);
+})
+
+
+let lisLibriRossi = document.querySelectorAll("#listaLibri li.red");
+console.log(lisLibriRossi);
+console.log(typeof lisLibriRossi);
+lisLibriRossi.forEach(li => {
+    console.log(li);
+})
