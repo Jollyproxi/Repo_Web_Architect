5d82ac48eab3d7908442081b89478fe7ca9145dd
Jollyproxi <davide.giolitti@edu-its.it>
2026-04-27 12:15:26 +0200

    ff

--- JAVASCRIPT/Lezione5_GestioneEventi/js/scriptPagReg.js
+++ JAVASCRIPT/Lezione5_GestioneEventi/js/scriptPagReg.js
@@ -1,11 +1,11 @@
 const h1Benv = document.querySelector("#benvenuto");
-const contBtn = document.querySelector("#contBtn");
+const contBtn= document.querySelector("#contBtn");
 const regConfermata = document.querySelector("#regConfermata");
-const boxUtenti = document.querySelector("#boxUtenti");
+const boxUtenti = document.querySelector("#boxUtenti"); 
 
-function recuperaUtenteReg() {
-    let userReg = JSON.parse(localStorage.getItem("nuovoUser"));
-    if (userReg != null) {
+function recuperaUtenteReg(){
+    let userReg = JSON.parse( localStorage.getItem("nuovoUser") );
+    if(userReg != null){
         h1Benv.textContent = `Grazie ${userReg.nome} per esserti registrato`;
         console.log(userReg);
     }
@@ -13,55 +13,50 @@ function recuperaUtenteReg() {
 }
 
 //L'evento DOMContentLoaded permette di eseguire una funzione nel momento in cui viene caricata una pagina
-document.addEventListener("DOMContentLoaded", function () {
-    if (recuperaUtenteReg() != null) {
-        contBtn.appendChild(creaBTNConferma());
+document.addEventListener("DOMContentLoaded", function(){
+    if(recuperaUtenteReg() != null ){
+        contBtn.appendChild(creaBTNConferma());  
     }
 });
 
-function creaBTNConferma() {
+function creaBTNConferma(){
     let btn = document.createElement("button");
     btn.textContent = "Conferma la tua registrazione";
-    btn.addEventListener("click", function () {
-        regConfermata.innerHTML = "<h3>Registrazione confermata!</h3>";
-        setTimeout(function () {
+    btn.addEventListener("click", function(){
+        regConfermata.innerHTML = "<h3>Registrazione Confermata </h3>";
+        setTimeout(function(){
             regConfermata.innerHTML = "";
             contBtn.removeChild(btn);
             creaBtnStampaListaUtenti();
             creaListaUtenti();
-        }, 1500)
-
-
-    })
+        }, 1500);
+    }
+    )
     return btn;
 }
 
-function creaBtnStampaListaUtenti() {
+function creaBtnStampaListaUtenti(qualcosa){
     let btn = document.createElement("button");
-    btn.textContent = "Stampa lista utenti registrati";
-    btn.addEventListener("click", function () {
-        console.log("Sto stampando la lista degli utenti registrati");
+    btn.textContent = "Stampa lista Utenti";
+    btn.addEventListener("click", function(){
+        console.log("Sto stampando la lista utenti");
     })
     boxUtenti.appendChild(btn);
 }
 
-
-let listaUtenti = [];
-
-function creaListaUtenti() {
-
-    // devo recuperare l'array di utenti già registrati, se esiste, altrimenti creo un array vuoto
-
+let listaUtenti = []
+function creaListaUtenti(){
+    //recupero l'array di utenti già salvato
     let listaEsistente = JSON.parse(localStorage.getItem("listaUtenti"));
-    if (listaEsistente != null) {
+    
+    if(listaEsistente != null){
         listaUtenti = listaEsistente;
     }
-
-
-    //recupero l'ultimo utente
-    let ultimoUtente = JSON.parse(localStorage.getItem("nuovoUser"));
-    listaUtenti.push(ultimoUtente);
-
-    //reinserisco
-    localStorage.setItem("listaUtenti", JSON.stringify(listaUtenti));
+        
+        //recupero l'ultimo utente
+        let ultimoUtente = JSON.parse(localStorage.getItem("nuovoUser"));
+        listaUtenti.push(ultimoUtente);
+        
+        //reinserisco l'array di utenti
+        localStorage.setItem("listaUtenti", JSON.stringify(listaUtenti));
 }
