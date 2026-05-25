c685ec1a441b0622ea2f9ee5321de396ba0ad58f
= <=>
2026-05-25 11:41:36 +0200

    up

--- REACT/project_3/src/App.jsx
+++ REACT/project_3/src/App.jsx
@@ -13,8 +13,31 @@ function App() {
   const beatles = [
     {
       id: 0,
-      nome: "Jhon Lennon",
-      description: "Il paroliere dei beatles"
+      nome: "John Lennon",
+      description: "Il paroliere dei beatles",
+      strumento: "voce, chitarra",
+      imgUrl: "https://m.media-amazon.com/images/M/MV5BMTYwMDE4MzgzMF5BMl5BanBnXkFtZTYwMDQzMzU3._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+      id: 1,
+      nome: "George Harrison",
+      description: "Il miglior beatle",
+      strumento: "chitarra",
+      imgUrl: "https://m.media-amazon.com/images/M/MV5BMTUyNjE0NzAzMl5BMl5BanBnXkFtZTYwMjQzMzU3._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+      id: 2,
+      nome: "Paul McCartney",
+      description: "Il beatle più simpatico",
+      strumento: "voce, chitarra",
+      imgUrl: "https://www.retetoscanaclassica.it/wp-content/uploads/2022/06/Paul-McCartney-by-Eric-Koch-for-Anefo.jpg"
+    },
+    {
+      id: 0,
+      nome: "Ringo Starr",
+      description: "Un batterista sottovalutato",
+      strumento: "batteria",
+      imgUrl: "https://townsquare.media/site/295/files/2012/11/ringo-Keystone-hutton-archives-getty.jpg?w=780&q=75"
     }
   ]
 
