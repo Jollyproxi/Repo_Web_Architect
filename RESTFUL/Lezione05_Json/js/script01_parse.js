88794e32978ad652d96cdaf0af5e0284af3ba40c
= <=>
2026-06-26 10:51:13 +0200

    up

---
+++ RESTFUL/Lezione05_Json/js/script01_parse.js
@@ -0,0 +1,30 @@
+//DESERIALIZZARE un JSON con il metodo PARSE
+let studenteJSON = '{"id": 1, "nome":"Maria Gialli", "presenza":true, "eta":25}';
+
+console.log(typeof studenteJSON);
+
+let studenteOBJ = JSON.parse(studenteJSON);
+console.log(studenteOBJ);
+//non avendo una classe a disposizione devo programmare "al buio"
+console.log(studenteOBJ.nome);
+
+/////////////////
+
+let docenteJSON = '{"id": 1, "nome":"Maria Gialli", "presenza":true, "eta":25}';
+
+class Docente{
+    constructor(id, nome, presenza, eta){
+        this.id = id;
+        this.nome = nome;
+        this.presenza = presenza;
+        this.eta = eta
+    }
+}
+
+let docenteOBJ = JSON.parse(docenteJSON);
+
+//Questo sotto è abbastanza una cafonata, si risolverà con JSON schema
+let docenteClasse = new Docente(docenteOBJ.id, docenteOBJ.nome, docenteOBJ.presenza, docenteOBJ.eta);
+
+console.log("Il docente si chiama: " + docenteClasse.nome);
+
